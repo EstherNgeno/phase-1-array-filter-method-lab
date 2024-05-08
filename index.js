fb5c74fc8e9e4ba5002c6e16fd4c68ca9d59d7ab
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-05-08 20:04:35 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers,string){
+    const sameDrivers = drivers.filter(function (driver){
+        return string.toUpperCase() === driver.toUpperCase() 
+  } );
+  return sameDrivers;
+}
+   console.log(sameDrivers);
+
+    
+
+    function fuzzyMatch(drivers, beginningLetters) {
+      const matchingDrivers = [];
+      for (let i = 0; i < drivers.length; i++) {
+          if (drivers[i].startsWith(beginningLetters)) {
+              matchingDrivers.push(drivers[i]);
+          }
+      }
+      return matchingDrivers;
+    } 
+
+    const  newdrivers = [
+      {
+        name: 'Bobby',
+        hometown: 'Pittsburgh' },
+      {
+        name: 'Sammy',
+        hometown: 'New York' } ,
+      {
+        name: 'Sally',
+        hometown: 'Cleveland' },
+      {
+        name: 'Annette',
+        hometown: 'Los Angeles' },
+      {
+        name: 'Bobby',
+        hometown: 'Tampa Bay' }
+    ];
+    
+    /*function matchName(name){
+       if (!name){
+        return false;
+          }
+       const nameFound = newdrivers.includes(name);
+        return nameFound;   
+    }
+    */
+    function matchName(drivers, name){
+      const newDrivers = drivers.filter(
+        function(driver){
+          return driver.name.toLowerCase() === name.toLowerCase();
+    
+        })
+      return newDrivers;
+    }
